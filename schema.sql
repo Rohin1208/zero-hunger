dd3099003a23b7fd7ae309199b8bc4a284d45de8
Rohin Vashishth <vashishthrohin@gmail.com>
2026-03-25 02:30:04 +0530

    Added food_listings table

--- schema.sql
+++ schema.sql
@@ -3,3 +3,10 @@ CREATE TABLE users (
     name VARCHAR(100),
     role VARCHAR(20) CHECK (role IN ('restaurant', 'ngo'))
 );
+CREATE TABLE food_listings (
+    id SERIAL PRIMARY KEY,
+    food_name VARCHAR(100),
+    quantity INTEGER,
+    expiry TIMESTAMP,
+    restaurant_id INTEGER REFERENCES users(id)
+);
